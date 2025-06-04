df6b85bcf662e9270a020d3f45e758009ddb2aca
wpo_dskim <dskim@wideplusone.com>
2025-06-04 23:59:08 +0900

    #7.1 Magic (06:31)

--- Home.py
+++ Home.py
@@ -1,9 +1,15 @@
 import streamlit as st
+from langchain.prompts import PromptTemplate
 
-st.title("Hello World!")
+st.write("Hello")
 
-st.subheader("Welcome to Streamlit")
+st.write([1,2,3,4])
 
-st.markdown("""
-    #### I love it
-""")
+st.write({"x": 1})
+
+st.write(PromptTemplate)
+
+p = PromptTemplate.from_template("xxxx")
+st.write(p)
+
+st.selectbox("Choose your model",("GPT-3", "GPT-4"))
