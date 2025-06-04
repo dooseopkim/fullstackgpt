246b4c65e3160efcb03882315026294173f1c820
wpo_dskim <dskim@wideplusone.com>
2025-06-05 00:06:37 +0900

    #7.2 Data Flow (06:18)

--- Home.py
+++ Home.py
@@ -1,15 +1,26 @@
 import streamlit as st
-from langchain.prompts import PromptTemplate
+from datetime import datetime
 
-st.write("Hello")
+"""
+#7.2 Data Flow (06:18)
 
-st.write([1,2,3,4])
+ - Data가 변경될 때마다 Python 파일 전체가 다시 실행될 것임.
+"""
 
-st.write({"x": 1})
+today = datetime.today().strftime("%H:%M:%S")
+st.header(today)
 
-st.write(PromptTemplate)
 
-p = PromptTemplate.from_template("xxxx")
-st.write(p)
+model = st.selectbox("Choose your model",("GPT-3", "GPT-4"))
 
-st.selectbox("Choose your model",("GPT-3", "GPT-4"))
+if model == "GPT-3":
+    st.write("cheap")
+else:
+    st.write("not cheap")
+    st.write(model)
+
+    name = st.text_input("What is your name?")
+    st.write(name)
+
+    value = st.slider("temperature", min_value=0.1, max_value=1.0)
+    st.write(value)
